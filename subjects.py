0232f0671f42b20206a396fcfb7c4384cf3a77ff
ChristianoEmmanuel <159271206+ChristianoEmmanuel@users.noreply.github.com>
2024-02-08 19:34:31 +0530

    Add files via upload

---
+++ subjects.py
@@ -0,0 +1,12 @@
+English   = 75
+Kannada   = 70
+Hindi     =50
+Biology   =75
+chemistry =85
+physics   =70
+totalscore=600
+totalmarks=English+Kannada+Hindi+Biology+chemistry+physics
+totalpercentage=totalmarks/totalscore*100
+print(totalmarks)
+print(totalpercentage)
+print(f"emmanuel scored 425 with 70.833%")
