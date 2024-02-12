b9718d258021ab628576c52adbd587cdd5854b43
ChristianoEmmanuel <159271206+ChristianoEmmanuel@users.noreply.github.com>
2024-02-12 20:04:19 +0530

    Add files via upload

---
+++ Input function.py
@@ -0,0 +1,19 @@
+
+name="emmanuel"
+num1=int(input("enter the num1"))
+num2=int(input("enter the num2"))
+print(num1*num2)
+
+num1=complex(input("enter the num1"))
+num2=complex(input("enter the num2"))
+print(num1-num2)
+
+num1=float(input("enter the num1"))
+num2=float(input("enter the num2"))
+print(num1*num2)
+
+num1=(input("enter the num1"))
+num2=(input("enter the num2"))
+print(num1+num2)
+
+
