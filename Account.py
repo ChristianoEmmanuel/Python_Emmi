99e501059dcf3c64270e34f0dc5b8b4a12ca7343
ChristianoEmmanuel <159271206+ChristianoEmmanuel@users.noreply.github.com>
2024-02-12 18:58:49 +0530

    Add files via upload

---
+++ Account.py
@@ -0,0 +1,11 @@
+amount=1000
+print("the total amout is",amount)
+debit=amount-20
+print("debit of 20rs "'debit')
+print(debit)
+credit=debit+50
+print("credit of 50rs "'credit')
+print(credit)
+print("========INTREST=======")
+simpleintrest=credit*1*10/100
+print(f'the overall intrest for {credit} amount is {simpleintrest}')
