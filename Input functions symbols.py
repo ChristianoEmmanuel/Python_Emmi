16acba3303dd27de86acac7a293dcfef6e0e77b8
ChristianoEmmanuel <159271206+ChristianoEmmanuel@users.noreply.github.com>
2024-02-13 18:46:57 +0530

    Add files via upload

---
+++ Input functions symbols.py
@@ -0,0 +1,11 @@
+
+a=int(input("enter the num1"))
+b=int(input("enter the num2"))
+print(f"the addition of {a} and {b}=",a+b)
+print(f"the subtraction of {a} and {b}=",a-b)
+print(f"the division of {a} and {b}=",a/b)
+print(f"the multiplication  of {a} and {b}=",a*b)
+print(f"the modulus of {a} and {b}=",a//b)
+print(f"the exponent of {a} and {b}=",a**b)
+print(f"the remainder of {a} and {b}=",a%b)
+
