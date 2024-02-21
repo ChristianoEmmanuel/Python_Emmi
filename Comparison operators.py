276f3cf99df3f30ed14bb2e309017402a71348b6
ChristianoEmmanuel <159271206+ChristianoEmmanuel@users.noreply.github.com>
2024-02-21 19:28:33 +0530

    Add files via upload

---
+++ Comparison operators.py
@@ -0,0 +1,49 @@
+a=50
+b=10
+print(a>b)
+
+a=50
+b=10
+print(a<b)
+
+a=50
+b=10
+print(a==b)
+
+a=50
+b=10
+print(a!=b)
+
+a=50
+b=10
+print(a>=b)
+
+a=50
+b=10
+print(a<=b)
+
+a=50
+b=10
+print(a is b)
+
+a=50
+b=10
+print(a is not b)
+
+a=50
+b=10
+print(a and b)
+
+a=50
+b=10
+print(a or b)
+
+a=50
+b=10
+print(not a)
+
+
+
+
+
+
